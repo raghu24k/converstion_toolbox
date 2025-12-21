5583de113ef41abd2e72a3c1f922e38ebb29b970
rkumar430@rku.ac.in <raghuwinderkumar24k@gmail.com>
2025-12-22 02:18:30 +0530

    fixed

--- frontend/src/components/UploadZone.jsx
+++ frontend/src/components/UploadZone.jsx
@@ -38,7 +38,7 @@ const UploadZone = () => {
         try {
             // Use window.location.hostname to allows access from other devices (e.g. phone)
             // This assumes backend is running on port 8080 on the same machine
-            const apiUrl = `https://converstion-toolbox.onrender.com`;
+            const apiUrl = `https://converstion-toolbox.onrender.com/api/convert`;
 
             const response = await axios.post(apiUrl, formData, {
                 responseType: 'blob', // Important for file download
